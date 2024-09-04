b25c0ec9f0985276e8886cdd5700ec9ded07ea49
Himanshu Paliwal <hpaliwal364@gmail.com>
2024-09-04 19:15:24 +0530

    page not found bug is fixed

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/pushPullLeg" element={<PushPullLegWorkoutPlan />} />
         <Route path="/broSplit" element={<BroSplitWorkoutPlan />} />
